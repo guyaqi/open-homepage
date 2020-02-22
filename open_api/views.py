9fb4d4def860b15d2ee71cf5a768014c1741d7bc
guyaqi <guyaqi5858@qq.com>
2020-02-22 16:34:13 +0800

    add blog_by_id

--- open_api/views.py
+++ open_api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from source_list import util as source_util
 from open_api import util as api_util
+from source_list.models import OpenBlog
 import json
 
 def all_blog(request):
@@ -12,6 +13,15 @@ def all_blog(request):
       blogs += api_util.getBlogFromGithub(source)
   
   return HttpResponse(json.dumps(blogs))
+
+def blog_by_id(request, source_id):
+  qs = OpenBlog.objects.filter(blog_id=source_id)
+  if len(qs) == 0:
+    return HttpResponse(json.dumps([]))
+  the_source = qs[0]
+  blogs = api_util.getBlogFromGithub(the_source)
+  return HttpResponse(json.dumps(blogs))
+
 
 def all_source(request):
   sources = source_util.all_sources_with_index()
