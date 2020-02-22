57262e128b7da1c782085c05291b917098f1ba5d
guyaqi <guyaqi5858@qq.com>
2020-02-22 17:06:57 +0800

    add blog_by_id_id

--- open_api/urls.py
+++ open_api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('all_blog', views.all_blog),
     path('blog/<int:source_id>', views.blog_by_id),
     path('all_source', views.all_source),
+    path('blog/<int:source_id>/<int:blog_id>', views.blog_by_id_id),
     
 ]
--- open_api/views.py
+++ open_api/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from source_list import util as source_util
 from open_api import util as api_util
 from source_list.models import OpenBlog
-import json
+import json, requests
 
 def all_blog(request):
   sources = source_util.all_sources()
@@ -27,3 +27,21 @@ def all_source(request):
   sources = source_util.all_sources_with_index()
   
   return HttpResponse(json.dumps(sources))
+
+def blog_by_id_id(request, source_id, blog_id):
+  qs = OpenBlog.objects.filter(blog_id=source_id)
+  if len(qs) == 0:
+    return HttpResponse(json.dumps([]))
+  the_source = qs[0]
+  blogs = api_util.getBlogFromGithub(the_source)
+  try:
+    the_blog = blogs[blog_id]
+  except IndexError as e:
+    return HttpResponse('blog not found')
+  
+  try:
+    res = requests.get(the_blog)
+    res = res.text()
+    return HttpResponse(res)
+  except Exception as e:
+    return HttpResponse('get blog failed')
