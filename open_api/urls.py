8bd174bd4da0860ce642c6393425d1a424943fb0
guyaqi <guyaqi5858@qq.com>
2020-02-22 16:42:36 +0800

    fix url pattern

--- open_api/urls.py
+++ open_api/urls.py
@@ -3,7 +3,7 @@ from open_api import views
 
 urlpatterns = [
     path('all_blog', views.all_blog),
-    path('blog/<source_id:int>', views.blog_by_id),
+    path('blog/<int:source_id>', views.blog_by_id),
     path('all_source', views.all_source),
     
 ]
