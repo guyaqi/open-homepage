048eeee824cfda610bd739db5d25a61ee1484de7
guyaqi <guyaqi5858@qq.com>
2020-02-22 15:59:42 +0800

    add blog_id

--- source_list/models.py
+++ source_list/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class OpenBlog(models.Model):
+  blog_id = models.AutoField(primary_key=True)
   blog_info = models.CharField(default='github', max_length=512)
